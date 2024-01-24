607ca2211bc75de47f004ee97951a11d318d4f95
karim-a-khaled <karim.a.s.khaled@gmail.com>
2024-01-24 13:24:05 +0300

    Add project files.

---
+++ Base.API/Extensions/ApplicationsServiceExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Base.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Base.API.Extensions
+{
+    public static class ApplicationsServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+            services.AddHttpContextAccessor();
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+
+            //services.AddCors(options => options.AddPolicy(name: "NgOrigins", policy => {
+            //    policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
+            //}));
+
+            return services;
+        }
+    }
+}
---
+++ Base.API/Program.cs
@@ -0,0 +1,19 @@
+using Base.API.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
