8bc3ac4f924b161b517ecec58a7c3532fe13e207
karim-a-khaled <karim.a.s.khaled@gmail.com>
2024-01-28 12:03:52 +0300

    added base entity

---
+++ Base.Entities/Entities/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base.Entities.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int CreatedById { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        public int ModifiedById { get; set; }
+        public DateTime ModificationDate { get; set; }
+    }
+}
